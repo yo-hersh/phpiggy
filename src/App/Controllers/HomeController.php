0e8c43419cb1b56bea281ca445c4061634a261bb
yo-hersh <96725301+yo-hersh@users.noreply.github.com>
2024-01-01 21:04:14 +0200

    generating a template engine

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use Framework\TemplateEngine;
+
 class HomeController
 {
+    private TemplateEngine $view;
+
+    public function __construct()
+    {
+        $this->view = new TemplateEngine();
+    }
     public function home()
     {
+        dd($this->view);
         echo "Home page";
     }
 }
