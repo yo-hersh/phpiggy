fae4b337ef11a42ee157c8bd9972aa5f060629a2
yo-hersh <96725301+yo-hersh@users.noreply.github.com>
2024-01-01 21:21:42 +0200

    adding a file paths to the template engine

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use Framework\TemplateEngine;
+use App\Config\Paths;
 
 class HomeController
 {
@@ -12,7 +13,7 @@ class HomeController
 
     public function __construct()
     {
-        $this->view = new TemplateEngine();
+        $this->view = new TemplateEngine(paths::VIEW);
     }
     public function home()
     {
--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -1,10 +1,12 @@
 <?php
 
-
 declare(strict_types=1);
 
 namespace Framework;
 
 class TemplateEngine
 {
+    public function __construct(private string $basePath)
+    {
+    }
 }
