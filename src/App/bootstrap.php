bd8b4c3d178735f27d8ac4b8aaa86461ed34edf7
yo-hersh <96725301+yo-hersh@users.noreply.github.com>
2024-01-05 02:21:09 +0200

    added about controller and view

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 include __DIR__ . '/../../vendor/autoload.php';
 
 use Framework\App;
-use App\Controllers\HomeController;
+use App\Controllers\{HomeController, AboutController};
 
 $app = new App();
 
@@ -13,5 +13,6 @@ $app = new App();
 // $app->router->add('/');
 
 $app->get('/', [HomeController::class, 'home']);
+$app->get('/about', [AboutController::class, 'about']);
 
 return $app;
