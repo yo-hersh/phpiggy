cd56b1472f61c5f1b4467d262b87d6f0128dc7a1
yo-hersh <96725301+yo-hersh@users.noreply.github.com>
2024-01-08 22:17:00 +0200

    move the adding routes logic into a difference file

---
+++ src/App/Config/Routes.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Config;
+
+use App\Controllers\{HomeController, AboutController};
+use Framework\App;
+
+class Routes
+{
+    public static function registerRoutes(App $app)
+    {
+        $app->get('/', [HomeController::class, 'home']);
+        $app->get('/about', [AboutController::class, 'about']);
+    }
+}
