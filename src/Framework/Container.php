de250cceb0b7ba7be1bcab74b17015ba8b0fd68f
yo-hersh <96725301+yo-hersh@users.noreply.github.com>
2024-01-15 15:48:47 +0200

    added the reflection class and add a value to the factory parameter

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -6,6 +6,7 @@ namespace Framework;
 
 use ReflectionClass;
 use Framework\Exception\ContainerException;
+use ReflectionNamedType;
 
 class Container
 {
@@ -14,7 +15,6 @@ class Container
     public function addDefinitions(array $newDefinitions): void
     {
         $this->definitions = [...$this->definitions, ...$newDefinitions];
-        // dd($this->definitions);
     }
 
     public function resolve(string $className)
@@ -25,6 +25,46 @@ class Container
         if (!$reflectionClass->isInstantiable()) {
             throw new ContainerException("class {$className} is not instantiable");
         }
-        dd($reflectionClass);
+
+        $constructor = $reflectionClass->getConstructor();
+
+        if (!$constructor) {
+            return new $className;
+        }
+
+        $params = $constructor->getParameters();
+
+        if (count($params) === 0) {
+            return new $className;
+        }
+
+        $dependencies = [];
+
+        foreach ($params as $param) {
+            $name = $param->getName();
+            $type = $param->getType();
+
+            if (!$type) {
+                throw new ContainerException("Failed to resolve class {$className} because {$name} has no type");
+            }
+            if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
+                throw new ContainerException("Failed to resolve class {$className} because invalid param name");
+            }
+
+            $dependencies[] = $this->get($type->getName());
+        }
+
+        return $reflectionClass->newInstanceArgs($dependencies);
+    }
+
+    public function get(string $id)
+    {
+        if (!array_key_exists($id, $this->definitions)) {
+            throw new ContainerException("Class {$id} does not exits in the container");
+        }
+
+        $factory = $this->definitions[$id];
+        $dependency = $factory();
+        return $dependency;
     }
 }
