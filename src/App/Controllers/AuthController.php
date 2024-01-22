cd4eda3c9123c54f5e64f3344b417c58d83bfcbd
yo-hersh <96725301+yo-hersh@users.noreply.github.com>
2024-01-22 14:43:08 +0200

    adding validator class

--- src/App/Controllers/AuthController.php
+++ src/App/Controllers/AuthController.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace App\Controllers;
 
 use Framework\TemplateEngine;
+use App\Services\ValidatorService;
 
 class AuthController
 {
-    public function __construct(private TemplateEngine $view)
-    {
+    public function __construct(
+        private TemplateEngine $view,
+        private ValidatorService $validatorService
+    ) {
     }
     public function registerView()
     {
--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 use Framework\TemplateEngine;
 use App\Config\Paths;
+use App\Services\ValidatorService;
 
 return [
-    TemplateEngine::class => fn () => new TemplateEngine(paths::VIEW)
+    TemplateEngine::class => fn () => new TemplateEngine(paths::VIEW),
+    ValidatorService::class => fn () => new ValidatorService(),
 ];
