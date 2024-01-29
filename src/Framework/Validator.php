f1623b230f6a99afb5cca3d027c25568936bd38c
yo-hersh <96725301+yo-hersh@users.noreply.github.com>
2024-01-29 22:28:00 +0200

    Added method to add a rule to the Validator class

--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use Framework\Contracts\RuleInterface;
+
 class Validator
 {
+    private array $rules = [];
+
+    public function addRule(string $alias, RuleInterface $rule)
+    {
+        $this->rules[$alias] = $rule;
+    }
     public function validate(array $data)
     {
         dd($data);
