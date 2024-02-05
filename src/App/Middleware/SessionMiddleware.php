ee6123d332b30565b5e60c6d7d40d93388ba92ca
yo-hersh <96725301+yo-hersh@users.noreply.github.com>
2024-02-05 20:37:52 +0200

    added and using session exception

--- src/App/Middleware/SessionMiddleware.php
+++ src/App/Middleware/SessionMiddleware.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace App\Middleware;
 
+use App\Exceptions\SessionException;
 use Framework\Contracts\MiddlewareInterface;
 
 class SessionMiddleware implements MiddlewareInterface
 {
     public function process(callable $next)
     {
+        if (session_status() === PHP_SESSION_ACTIVE) {
+            throw new SessionException("Session already started");
+        }
+
+        if (headers_sent()) {
+            throw new SessionException("Headers already sent");
+        }
         session_start();
         $next();
     }
