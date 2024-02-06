1142845b17610a369f42150000bfa7167b46e9fd
yo-hersh <96725301+yo-hersh@users.noreply.github.com>
2024-02-06 14:44:22 +0200

    FlashMiddleware
    added a middleware for flashing into middlewares, trying by flashing errors

--- src/App/Config/Middleware.php
+++ src/App/Config/Middleware.php
@@ -5,7 +5,12 @@ declare(strict_types=1);
 namespace App\Config;
 
 use Framework\App;
-use App\Middleware\{SessionMiddleware, TemplateDataMiddleware, ValidationExceptionMiddleware};
+use App\Middleware\{
+    FlashMiddleware,
+    SessionMiddleware,
+    TemplateDataMiddleware,
+    ValidationExceptionMiddleware
+};
 
 class Middleware
 {
@@ -25,6 +30,7 @@ class Middleware
         */
         $app->addMiddleware(TemplateDataMiddleware::class);
         $app->addMiddleware(ValidationExceptionMiddleware::class);
+        $app->addMiddleware(FlashMiddleware::class);
         $app->addMiddleware(SessionMiddleware::class);
     }
 }
