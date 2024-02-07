4ddb751629323addd3e57717757c9450ba82cc04
yo-hersh <96725301+yo-hersh@users.noreply.github.com>
2024-02-07 15:06:27 +0200

    Insert the errors into the fields

--- src/App/views/register.php
+++ src/App/views/register.php
@@ -1,17 +1,27 @@
 <?php include $this->resolve('partials/_header.php'); ?>
 
 <section class="max-w-2xl mx-auto mt-12 p-4 bg-white shadow-md border border-gray-200 rounded">
-    <?php var_dump($flash['errors'] ?? []) ?>
+    <?php $errors = $flash['errors'] ?? []; ?>
     <form method="POST" class="grid grid-cols-1 gap-6">
         <!-- Email -->
         <label class="block">
             <span class="text-gray-700">Email address</span>
             <input name="email" type="email" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="john@example.com" />
+            <?php if (array_key_exists('email', $errors)) : ?>
+                <div class="bg-grey-100 mt-2 p-2 text-red-500">
+                    <?php foreach ($errors['email'] as $error) echo e($error); ?>
+                </div>
+            <?php endif; ?>
         </label>
         <!-- Age -->
         <label class="block">
             <span class="text-gray-700">Age</span>
             <input name="age" type="number" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="" />
+            <?php if (array_key_exists('age', $errors)) : ?>
+                <div class="bg-grey-100 mt-2 p-2 text-red-500">
+                    <?php foreach ($errors['age'] as $error) echo e($error); ?>
+                </div>
+            <?php endif; ?>
         </label>
         <!-- Country -->
         <label class="block">
@@ -27,16 +37,31 @@
         <label class="block">
             <span class="text-gray-700">Social Media URL</span>
             <input name="social_media_url" type="text" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="" />
+            <?php if (array_key_exists('social_media_url', $errors)) : ?>
+                <div class="bg-grey-100 mt-2 p-2 text-red-500">
+                    <?php foreach ($errors['social_media_url'] as $error) echo e($error); ?>
+                </div>
+            <?php endif; ?>
         </label>
         <!-- Password -->
         <label class="block">
             <span class="text-gray-700">Password</span>
             <input name="password" type="password" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="" />
+            <?php if (array_key_exists('password', $errors)) : ?>
+                <div class="bg-grey-100 mt-2 p-2 text-red-500">
+                    <?php foreach ($errors['password'] as $error) echo e($error); ?>
+                </div>
+            <?php endif; ?>
         </label>
         <!-- Confirm Password -->
         <label class="block">
             <span class="text-gray-700">Confirm Password</span>
             <input name="confirm_password" type="password" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="" />
+            <?php if (array_key_exists('confirm_password', $errors)) : ?>
+                <div class="bg-grey-100 mt-2 p-2 text-red-500">
+                    <?php foreach ($errors['confirm_password'] as $error) echo e($error); ?>
+                </div>
+            <?php endif; ?>
         </label>
         <!-- Terms of Service -->
         <div class="block">
@@ -45,10 +70,15 @@
                     <label class="inline-flex items-center">
                         <input name="tos" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50" type="checkbox" />
                         <span class="ml-2">I accept the terms of service.</span>
-                    </label>
                 </div>
             </div>
         </div>
+        <?php if (array_key_exists('tos', $errors)) : ?>
+            <div class="bg-grey-100 mt-2 p-2 text-red-500">
+                <?php foreach ($errors['tos'] as $error) echo e($error); ?>
+            </div>
+        <?php endif; ?>
+        </label>
         <button type="submit" class="block w-full py-2 bg-indigo-600 text-white rounded">
             Submit
         </button>
