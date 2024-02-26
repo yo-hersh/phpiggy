55b997e3ebf1ff8b19aa185d8257448f49ee34b4
yo-hersh <96725301+yo-hersh@users.noreply.github.com>
2024-02-26 11:58:08 +0200

    Adding database connection class and run it from the cli.php file using composer scripts

---
+++ cli.php
@@ -0,0 +1,13 @@
+<?php
+
+include __DIR__ . "/src/Framework/Database.php";
+
+use Framework\Database;
+
+$db = new Database('mysql', [
+    'host' => 'localhost',
+    'port' => 3306,
+    'database' => 'phpiggy'
+], 'root', '');
+
+echo 'Connection established';
