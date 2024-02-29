efa505a2198577f5b26e2e1f1da87148819e883f
yo-hersh <96725301+yo-hersh@users.noreply.github.com>
2024-02-29 15:03:07 +0200

    Added sql file write a query to create users table

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `users`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `age` TINYINT(3) UNSIGNED NOT NULL,
+    `country` VARCHAR(255) NOT NULL,
+    `social_media_url` VARCHAR(255) NOT NULL,
+    `created_at` DATETIME NOT NULL default CURRENT_TIMESTAMP,
+    `updated_at` DATETIME NOT NULL default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    UNIQUE KEY (`email`)
+);
+-- ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
