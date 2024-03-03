4802c1f8ae0196e631aa342d53408dfa9b74df33
yo-hersh <96725301+yo-hersh@users.noreply.github.com>
2024-03-03 14:58:03 +0200

    Added container definition for database class

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -5,8 +5,19 @@ declare(strict_types=1);
 use Framework\TemplateEngine;
 use App\Config\Paths;
 use App\Services\ValidatorService;
+use Framework\Database;
 
 return [
     TemplateEngine::class => fn () => new TemplateEngine(paths::VIEW),
     ValidatorService::class => fn () => new ValidatorService(),
+    Database::class => fn () => new Database(
+        'mysql',
+        [
+            'host' => 'localhost',
+            'port' => 3306,
+            'dbname' => 'phpiggy',
+        ],
+        'root',
+        ''
+    )
 ];
