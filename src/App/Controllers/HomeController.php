70ddf44a3540f0256263538393d18583383498da
yo-hersh <96725301+yo-hersh@users.noreply.github.com>
2024-03-28 21:24:05 +0200

    Added transaction  from the ddb into the home page

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -4,21 +4,25 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+use App\Services\TransactionService;
 use Framework\TemplateEngine;
 use App\Config\Paths;
 
 class HomeController
 {
-    public function __construct(private TemplateEngine $view)
-    {
+    public function __construct(
+        private TemplateEngine $view,
+        private TransactionService $transactionService
+    ) {
     }
     public function home()
     {
+        $transactions = $this->transactionService->getTransactionsByUser();
         echo $this->view->render(
             'index.php',
             [
-                'username' => 'Yosef Hershkovitz',
                 'title' => 'Home',
+                'transactions' => $transactions
             ]
         );
     }
